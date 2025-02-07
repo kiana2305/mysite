8f552da3666559c94530834ee27929b58d102ceb
kiana <kianacs502023@gmail.com>
2025-02-07 13:45:29 +0330

    I made static and media
    STATIC_URL, STATIC_ROOT, MEDIA_URL and MEDIA_ROOT have been made

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path ,include 
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -22,5 +24,9 @@ urlpatterns = [
     # path ('url address' , 'view')
     path('' ,include('website.urls'))
     #path('blog/',include('blog.urls'))
+   
  
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
